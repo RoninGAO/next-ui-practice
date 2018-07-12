8eff7b23d2997cfd69504f0e20b1b2b26003a2d2
RoninGAO <minkou.lee@gmail.com>
2018-07-13 03:35:46 +0800

    add Simple-1 & Simple-2

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 *** This repo is Cisco NeXt UI - Sample ***
+
+### source by https://github.com/NeXt-UI ###
+### tutorials by https://github.com/NeXt-UI/next-tutorials  ###
+### reference by https://github.com/NeXt-UI/next-bower ### 
---
+++ app/app.js
@@ -0,0 +1,39 @@
+(function(nx) {
+
+    // instantiate next app
+    var app = new nx.ui.Application();
+
+    // configuration object for next
+    var topologyConfig = {
+        showNavigation: true,
+        width: 1000,
+        height: 500,
+        // special configuration for nodes
+        "nodeConfig": {
+            "label": "model.name",
+            "iconType": "router"
+        },
+        // special configuration for links
+        "linkConfig": {
+            "linkType": "curve"
+        },
+        // if true, the nodes' icons are shown, otherwise a user sees a dot instead
+        "showIcon": true,
+        // automatically compute the position of nodes
+        "dataProcessor": "force"
+
+    };
+
+    // instantiate Topology class
+    var topology = new nx.graphic.Topology(topologyConfig);
+
+    // load topology data from app/data.js
+    topology.data(topologyData);
+
+    // bind the topology object to the app
+    topology.attach(app);
+
+    // app must run inside a specific container. In our case this is the one with id="topology-container"
+    app.container(document.getElementById("topology-container"));
+
+})(nx);
